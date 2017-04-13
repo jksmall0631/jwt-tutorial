857726088679a733d1eace25c5472909f4901931
Joshua Small <jksmall0631@gmail.com>
2017-04-12 19:13:53 -0600

    finish tutorial

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,20 @@ import Auth from './Auth';
 
 class App extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = {
-      trains: []
-    };
+  super(props);
+  this.state = {
+    authStatus: {
+      loggedIn: false,
+      username: '',
+      token: ''
+    },
+    trains: []
+  };
+  this.updateTrains = this.updateTrains.bind(this);
+};
 
-    this.updateTrains = this.updateTrains.bind(this);
+  updateAuthStatus(authStatus, redirect) {
+    this.setState({authStatus}, browserHistory.push(`/${redirect}/`));
   }
 
   fetchTrains() {
@@ -25,6 +33,19 @@ class App extends React.Component {
   }
 
   componentDidMount() {
+    let token = localStorage.getItem('token');
+    let username = localStorage.getItem('username');
+
+    if (token && username) {
+      this.setState({
+        authStatus: {
+          loggedIn: true,
+          username,
+          token
+        }
+      })
+    }
+
     this.fetchTrains();
   }
 
@@ -34,13 +55,20 @@ class App extends React.Component {
 
   render () {
     const { trains } = this.state;
+    const { authStatus } = this.state;
 
     return (
       <div>
-      <h1>Big Metro City Choo-Choo Train Authority</h1>
+        <h1>Big Metro City Choo-Choo Train Authority</h1>
+        <Auth
+          username={authStatus.username}
+          updateAuthStatus={this.updateAuthStatus}
+        />
         {React.cloneElement(
           this.props.children,
-          { 
+          {
+            authStatus,
+            updateAuthStatus: this.updateAuthStatus,
             trains,
             updateTrains: this.updateTrains
           }
